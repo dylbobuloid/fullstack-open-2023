ea785789e5baccce4d0437172e22e7725d6e306a
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2024-09-12 16:50:04 +0100

    Completed phonebook step 6 exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
-const Filter = ({newSearch, handleSearchChange}) => {
+const Filter = ({ newSearch, handleSearchChange }) => {
 
-  return(
+  return (
     <div>
-    filter shown with<input value={newSearch}
-      onChange={handleSearchChange}>
-    </input>
+      filter shown with<input value={newSearch}
+        onChange={handleSearchChange}>
+      </input>
     </div>
   )
 
 }
 
+const PersonForm = ({ addNewPerson, newName, handlePersonChange, newNumber, handleNameChange }) => {
 
-const PersonForm = ({addNewPerson, newName, handlePersonChange, newNumber, handleNameChange }) => {
-
-  return(
+  return (
 
     <form onSubmit={addNewPerson}>
       <div>
@@ -43,27 +43,34 @@ const PersonForm = ({addNewPerson, newName, handlePersonChange, newNumber, handl
 
 }
 
-const Persons = ({persons, newSearch}) => {
-  return(
-    persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase())).map(person =>
+const Persons = ({ persons, newSearch }) => {
+  return (
+    persons.filter(person => person.name.toLowerCase()
+    .includes(newSearch.toLowerCase()))
+    .map(person =>
       <p key={person.name}> {person.name} {person.number}</p>
     )
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: '1', name: 'Arto Hellas', number: '0700'},
-    { id: '2', name: 'John Pork', number: '0400' },
-    { id: '3', name: 'Monquavious Tangerine', number: '079ooahah' },
-    { id: '4', name: 'Freakbob', number: '8008135' },
-    { id: '5', name: 'Monquavious Coconut', number: '00000001' },
-
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
 
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  
   const handleSearchChange = (event) => {
     setNewSearch(event.target.value)
   }
@@ -81,55 +88,55 @@ const App = () => {
   const addNewPerson = (event) => {
     event.preventDefault()
 
-      const personObject = {
-        id: persons.length + 1,
-        name: newName,
-        number: newNumber
-        
-      }
+    const personObject = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber
 
-      console.log(persons)
-      console.log("This is the new name", newName)
-      console.log("This is the new Person Object", personObject)
+    }
+
+    console.log(persons)
+    console.log("This is the new name", newName)
+    console.log("This is the new Person Object", personObject)
 
 
-      const nameExists = persons.some(person => person.name === newName)
+    const nameExists = persons.some(person => person.name === newName)
 
-      console.log("The name exists in the phonebook", nameExists)
+    console.log("The name exists in the phonebook", nameExists)
 
-      if(nameExists){
-        window.alert(`${newName} is already added to phonebook`)
-        setNewName('')
-        setNewNumber('')
-        return
-      }
+    if (nameExists) {
+      window.alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      setNewNumber('')
+      return
+    }
 
-      else{
-        setPersons(persons.concat(personObject))
-        setNewName('')
-        setNewNumber('')
-      }
+    else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
 
   }
 
   return (
     <div>
-      <div>debug name: {newName}</div>
+      {/* <div>debug name: {newName}</div>
       <div>debug number: {newNumber}</div>
-      <div>debug search: {newSearch}</div>
+      <div>debug search: {newSearch}</div> */}
 
-  
+
 
       <h2>Phonebook</h2>
       <Filter newSearch={newSearch} handleSearchChange={handleSearchChange} />
 
       <h3>Add a new </h3>
 
-      <PersonForm addNewPerson ={addNewPerson} newName={newName} handlePersonChange={handlePersonChange}
-       newNumber={newNumber} handleNameChange={handleNameChange} />
+      <PersonForm addNewPerson={addNewPerson} newName={newName} handlePersonChange={handlePersonChange}
+        newNumber={newNumber} handleNameChange={handleNameChange} />
 
       <h2>Numbers</h2>
-      <Persons persons={persons} newSearch={newSearch}/>
+      <Persons persons={persons} newSearch={newSearch} />
 
     </div>
   )
