2fc0a693ec375b396b047a855af0cb4c73feff59
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-06-23 18:54:41 +0100

    3.6 Phonebook backend step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,36 +3,36 @@ const app = express()
 
 app.use(express.json())
 
-let persons = 
-[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons =
+    [
+        {
+            "id": "1",
+            "name": "Arto Hellas",
+            "number": "040-123456"
+        },
+        {
+            "id": "2",
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523"
+        },
+        {
+            "id": "3",
+            "name": "Dan Abramov",
+            "number": "12-43-234345"
+        },
+        {
+            "id": "4",
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122"
+        }
+    ]
 
 app.get('/info', (request, response) => {
     const personsCount = persons.length
 
     const time = new Date()
 
-    response.send (
+    response.send(
         `<p>Phonebook has info for ${personsCount} people</p>
         <p>${time}</p>`
     )
@@ -63,23 +63,30 @@ app.get('/api/persons/:id', (request, response) => {
 
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
+    const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: Math.floor(Math.random() * 10000) + 1,
-  }
+    } else if (persons.find(person => person.name === body.name)) {
+        console.log("matching person found")
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
 
-  persons = persons.concat(person)
+    }
 
-  response.json(person)
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 10000) + 1,
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 
