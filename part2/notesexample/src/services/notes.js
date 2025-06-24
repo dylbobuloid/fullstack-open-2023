e08070965a2340a661b3197e9b84a45fd0bb95cc
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-06-24 02:30:28 +0100

    Configured CORS to connect front-end to back-end (5)

--- part2/notesexample/src/services/notes.js
+++ part2/notesexample/src/services/notes.js
@@ -1,21 +1,13 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3001/api/notes'
+    : '/api/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
-
-
-// const getAll = () => {
-//   const request = axios.get(baseUrl)
-//   const nonExisting = {
-//     id: 10000,
-//     content: 'This note is not saved to server',
-//     important: true,
-//   }
-//   return request.then(response => response.data.concat(nonExisting))
-// }
 
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
