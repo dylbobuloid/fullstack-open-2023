fa9ccfc72516b0f1b2459972dfac491dd5061b8a
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-06-24 15:36:46 +0100

    Small changes to index file

--- part3/parta/index.js
+++ part3/parta/index.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(cors())
 
 let notes = [
     {
