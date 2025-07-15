2cb2c5c71b5e6e2e3d5a2384c098ce5a5f8d2765
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-07-15 15:09:34 +0100

    3.16: Phonebook database, step 4 complete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,7 +31,7 @@ app.get('/info', (request, response) => {
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
 
     console.log("Heres the full request", request.params)
     Person.findByIdAndDelete(request.params.id)
@@ -43,13 +43,11 @@ app.delete('/api/persons/:id', (request, response) => {
                 response.json(404).send({ error: "Person not found" })
             }
         })
-        .catch(error => {
-            console.error("Error deleting person:", error.message)
-            response.status(400).send({ error: "Malformatted ID" })
-        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
@@ -57,12 +55,8 @@ app.get('/api/persons/:id', (request, response) => {
             response.status(404).end()
         }
     })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: "malformed id" })
-        })
+        .catch(error => next(error))
 })
-
 
 
 app.post('/api/persons', (request, response) => {
@@ -116,6 +110,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
