b47f981dd09c647a240e495e2252a69eea4078ce
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2025-08-06 01:53:51 +0100

    3.20*: Phonebook database, step 8 Complete

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -21,7 +21,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function(v){
+        return /\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Phone number required']
+  },
 })
 
 personSchema.set('toJSON', {
